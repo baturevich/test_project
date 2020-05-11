e8fc12fbc9fbb4478029a948b8d4c7c424c9a6c5
k_batur <batureevich@yandex.ru>
2020-05-11 14:58:28 +0500

    Updates

---
+++ precache-manifest.f180026c7ff1afcd84e193b76a84cb10.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e650fdc066a934da7c34c5daa00ddfd1",
+    "url": "/baturevich/SocNet/index.html"
+  },
+  {
+    "revision": "4db84e596ac8cb38c876",
+    "url": "/baturevich/SocNet/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a850e59fa013a6af602b",
+    "url": "/baturevich/SocNet/static/css/3.79ddfa82.chunk.css"
+  },
+  {
+    "revision": "e5c15f469ae98a8abd68",
+    "url": "/baturevich/SocNet/static/css/4.08d31d24.chunk.css"
+  },
+  {
+    "revision": "26846b094257d6c4c95f",
+    "url": "/baturevich/SocNet/static/css/main.a1d92393.chunk.css"
+  },
+  {
+    "revision": "4db84e596ac8cb38c876",
+    "url": "/baturevich/SocNet/static/js/2.d16457db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/baturevich/SocNet/static/js/2.d16457db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a850e59fa013a6af602b",
+    "url": "/baturevich/SocNet/static/js/3.b047a182.chunk.js"
+  },
+  {
+    "revision": "e5c15f469ae98a8abd68",
+    "url": "/baturevich/SocNet/static/js/4.88676a9f.chunk.js"
+  },
+  {
+    "revision": "26846b094257d6c4c95f",
+    "url": "/baturevich/SocNet/static/js/main.5a5ae0a3.chunk.js"
+  },
+  {
+    "revision": "08b7becf32d73a1593ec",
+    "url": "/baturevich/SocNet/static/js/runtime-main.59eb076e.js"
+  },
+  {
+    "revision": "951c2859d33608ef454e3bcb3e0d50b2",
+    "url": "/baturevich/SocNet/static/media/Loading.951c2859.gif"
+  },
+  {
+    "revision": "0304926bc70d15e43891b44191964016",
+    "url": "/baturevich/SocNet/static/media/login_img.0304926b.png"
+  }
+]);
